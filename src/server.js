7e25f4a95604b549cf69ae855fc1572d3b9e8ed4
NSZer0 <bradatima@gmail.com>
2024-05-08 18:22:47 -0500

    Modified the server start code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const { PORT = 4000 } = process.env;
+const PORT = process.env.PORT || 5001;
 
 const app = require("./app");
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
+app.listen(PORT, listener);
+
+// knex.migrate
+//   .latest()
+//   .then((migrations) => {
+//     console.log("migrations", migrations);
+//     app.listen(PORT, listener);
+//   })
+//   .catch(console.error);
